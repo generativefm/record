f8f1dd431d0d07a9d7f9505941f4d1d1147d5e82
metalex9 <alexbainter@gmail.com>
2020-10-18 16:10:04 -0500

    Retry failed sample index loading

--- src/samples/library.js
+++ src/samples/library.js
@@ -1,10 +1,29 @@
 import createProvider from '@generative-music/web-provider';
 import createLibrary from '@generative-music/web-library';
 
-const lazyOggSamples = () =>
-  import('@generative-music/samples-alex-bainter/src/ogg');
-const lazyMp3Samples = () =>
-  import('@generative-music/samples-alex-bainter/src/mp3');
+const makeLazyRetryable = (importFn) => {
+  const lazyRetry = (numAttempts = 0) =>
+    new Promise((resolve, reject) => {
+      importFn()
+        .then(resolve)
+        .catch((error) => {
+          if (numAttempts >= 3) {
+            reject(error);
+          }
+          setTimeout(() => {
+            lazyRetry(numAttempts + 1);
+          }, 1000);
+        });
+    });
+  return lazyRetry;
+};
+
+const lazyOggSamples = makeLazyRetryable(() =>
+  import('@generative-music/samples-alex-bainter/src/ogg')
+);
+const lazyMp3Samples = makeLazyRetryable(() =>
+  import('@generative-music/samples-alex-bainter/src/mp3')
+);
 
 const provider = createProvider();
 const audio = document.createElement('audio');
