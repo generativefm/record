cca82a6427cd00f4101d1dcab760ea663894b9d6
metalex9 <alexbainter@gmail.com>
2020-10-18 23:22:21 -0500

    Create reusable modal component

--- src/layout/overlay.component.jsx
+++ src/layout/overlay.component.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
+import useIsFooterVisible from './use-is-footer-visible.hook';
 import './overlay.styles.scss';
 
-const Overlay = ({ children }) => (
-  <CSSTransition in={true} classNames="overlay-" timeout={200} appear>
-    <div className="overlay">{children}</div>
-  </CSSTransition>
-);
+const Overlay = ({ children }) => {
+  const isFooterVisible = useIsFooterVisible();
+  return (
+    <CSSTransition in={true} classNames="overlay-" timeout={200} appear>
+      <div
+        className={`overlay${isFooterVisible ? ' overlay--above-footer' : ''}`}
+      >
+        {children}
+      </div>
+    </CSSTransition>
+  );
+};
 
 Overlay.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ src/modal/modal-header.component.js
@@ -0,0 +1,6 @@
+import makeWrapper from '../common/make-wrapper.hoc';
+import './modal-header.styles.scss';
+
+const ModalHeader = makeWrapper({ className: 'modal__header' });
+
+export default ModalHeader;
